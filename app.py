e46d74fc7416419125747cde2467a7d3754a7ebe
Paulo Jonas <pjonas21@gmail.com>
2024-10-22 16:47:08 -0300

    fix: botao de fechar modal, limpa campo de pasta de arquivos

--- app.py
+++ app.py
@@ -16,23 +16,28 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.START
     page.window.icon = "assets\\icons\\pdf.ico"
     page.padding = ft.padding.all(10)
-    page.window.height = 400
-    page.window.width = 650
+    page.window.height = 300
+    page.window.width = 600
 
     folder_path = ft.TextField(label="Caminho da pasta", width=400)
     file_name = ft.TextField(label="Nome do arquivo de saída", width=400)
-    status_label = ft.Text("", width=400)
 
     dialog = ft.FilePicker(on_result = on_pick_folder)
 
     select_folder_btn = ft.ElevatedButton(
         text="Selecionar pasta",
-        on_click=lambda _: dialog.get_directory_path()
+        on_click=lambda _: dialog.get_directory_path(),
+        style=ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(radius=10)
+        )
     )
 
     combine_btn = ft.ElevatedButton(
         text="Combinar PDFs",
-        on_click = lambda _: merge_pdf(folder_path.value, file_name.value, page)
+        on_click = lambda _: merge_pdf(folder_path.value, file_name.value, page),
+        style=ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(radius=10)
+        )
     )
 
     folder_row = ft.Row(
@@ -42,7 +47,53 @@ def main(page: ft.Page):
         ]
     )
 
+    def handle_close(e):
+        page.close(dlg_modal_success)
+
+    def handle_close_error(e):
+        page.close(dlg_modal_folder_error)
+
+    def handle_close_file_error(e):
+        page.close(dlg_modal_file_error)
+
+    dlg_modal_success = ft.AlertDialog(
+        modal = True,
+        title=ft.Text("Resultado"),
+        content=ft.Text("Operação realizada com sucesso.", style=ft.TextStyle(color="green")),
+        actions=[
+            ft.TextButton("OK", on_click=handle_close)
+        ],
+        actions_alignment=ft.MainAxisAlignment.END
+    )
+
+    dlg_modal_folder_error = ft.AlertDialog(
+        modal = True,
+        title=ft.Text("Erro"),
+        content=ft.Text("O caminho não é uma pasta válida.", style=ft.TextStyle(color="red")),
+        actions=[
+            ft.TextButton("OK", on_click=handle_close_error)
+        ],
+        actions_alignment=ft.MainAxisAlignment.END
+    )
+
+    dlg_modal_file_error = ft.AlertDialog(
+        modal = True,
+        title=ft.Text("Erro"),
+        content=ft.Text("Nenhum arquivo encontrado.", style=ft.TextStyle(color="red")),
+        actions=[
+            ft.TextButton("OK", on_click=handle_close_file_error)
+        ],
+        actions_alignment=ft.MainAxisAlignment.END
+    )
+
     page.overlay.append(dialog)
-    page.add(folder_row, file_name, combine_btn, status_label)
+    page.add(
+        folder_row, 
+        file_name, 
+        combine_btn, 
+        dlg_modal_folder_error,
+        dlg_modal_file_error,
+        dlg_modal_success
+        )
 
 ft.app(target=main)
--- merger.py
+++ merger.py
@@ -4,21 +4,13 @@ from PyPDF2 import PdfMerger
 
 def merge_pdf(folder_path: str, file_name: str, page: ft.Page):
     if not os.path.isdir(folder_path):
-        page.controls[-1].value = "O caminho não é uma pasta válida."
-        page.controls[-1].size = 16
-        page.controls[-1].color = ft.colors.RED_700
-        page.controls[-1].weight=ft.FontWeight.BOLD
-        page.controls[-1].italic=True
+        page.open(page.controls[-3])
         page.update()
         return
     
     pdf_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.pdf')])
     if not pdf_files:
-        page.controls[-1].value = "Nenhum arquivo encontrado."
-        page.controls[-1].size = 16
-        page.controls[-1].color = ft.colors.ORANGE_700
-        page.controls[-1].weight=ft.FontWeight.BOLD
-        page.controls[-1].italic=True
+        page.open(page.controls[-2])
         page.update()
         return
     
@@ -34,11 +26,8 @@ def merge_pdf(folder_path: str, file_name: str, page: ft.Page):
     merger.write(output_path)
     merger.close()
 
-    page.controls[-1].value = f"Sucesso. \nCaminho {output_path}"
-    page.controls[-1].size = 16
-    page.controls[-1].color = ft.colors.GREEN_700
-    page.controls[-1].weight=ft.FontWeight.BOLD
-    page.controls[-1].italic=True
+    page.open(page.controls[-1])
+    page.controls[0].controls[0].value = ""
     page.update()
     return
     
