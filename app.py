3b0074900fd7e82468548301b5f1e9d1eff65063
Paulo Jonas <pjonas21@gmail.com>
2024-10-23 21:22:01 -0300

    refatoração de código

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ from merger import merge_pdf
 
 
 def main(page: ft.Page):
-
     def on_pick_folder(e):
         if e.data:
             folder_data = json.loads(e.data)
             folder_path.value = f"{folder_data['path']}"
             page.update()
-    
 
     page.title = "PDF Merger"
     page.vertical_alignment = ft.MainAxisAlignment.START
@@ -22,7 +20,7 @@ def main(page: ft.Page):
     folder_path = ft.TextField(label="Caminho da pasta", width=400)
     file_name = ft.TextField(label="Nome do arquivo de saída", width=400)
 
-    dialog = ft.FilePicker(on_result = on_pick_folder)
+    dialog = ft.FilePicker(on_result=on_pick_folder)
 
     select_folder_btn = ft.ElevatedButton(
         text="Selecionar pasta",
@@ -34,7 +32,7 @@ def main(page: ft.Page):
 
     combine_btn = ft.ElevatedButton(
         text="Combinar PDFs",
-        on_click = lambda _: merge_pdf(folder_path.value, file_name.value, page),
+        on_click=lambda _: merge_pdf(folder_path.value, file_name.value, page),
         style=ft.ButtonStyle(
             shape=ft.RoundedRectangleBorder(radius=10)
         )
@@ -50,14 +48,8 @@ def main(page: ft.Page):
     def handle_close(e):
         page.close(dlg_modal_success)
 
-    def handle_close_error(e):
-        page.close(dlg_modal_folder_error)
-
-    def handle_close_file_error(e):
-        page.close(dlg_modal_file_error)
-
     dlg_modal_success = ft.AlertDialog(
-        modal = True,
+        modal=True,
         title=ft.Text("Resultado"),
         content=ft.Text("Operação realizada com sucesso.", style=ft.TextStyle(color="green")),
         actions=[
@@ -67,33 +59,34 @@ def main(page: ft.Page):
     )
 
     dlg_modal_folder_error = ft.AlertDialog(
-        modal = True,
+        modal=True,
         title=ft.Text("Erro"),
         content=ft.Text("O caminho não é uma pasta válida.", style=ft.TextStyle(color="red")),
         actions=[
-            ft.TextButton("OK", on_click=handle_close_error)
+            ft.TextButton("OK", on_click=handle_close)
         ],
         actions_alignment=ft.MainAxisAlignment.END
     )
 
     dlg_modal_file_error = ft.AlertDialog(
-        modal = True,
+        modal=True,
         title=ft.Text("Erro"),
         content=ft.Text("Nenhum arquivo encontrado.", style=ft.TextStyle(color="red")),
         actions=[
-            ft.TextButton("OK", on_click=handle_close_file_error)
+            ft.TextButton("OK", on_click=handle_close)
         ],
         actions_alignment=ft.MainAxisAlignment.END
     )
 
     page.overlay.append(dialog)
     page.add(
-        folder_row, 
-        file_name, 
-        combine_btn, 
+        folder_row,
+        file_name,
+        combine_btn,
         dlg_modal_folder_error,
         dlg_modal_file_error,
         dlg_modal_success
-        )
+    )
+
 
 ft.app(target=main)
--- merger.py
+++ merger.py
@@ -2,18 +2,19 @@ import os
 import flet as ft
 from PyPDF2 import PdfMerger
 
+
 def merge_pdf(folder_path: str, file_name: str, page: ft.Page):
     if not os.path.isdir(folder_path):
         page.open(page.controls[-3])
         page.update()
         return
-    
+
     pdf_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.pdf')])
     if not pdf_files:
         page.open(page.controls[-2])
         page.update()
         return
-    
+
     merger = PdfMerger()
     for pdf in pdf_files:
         pdf_path = os.path.join(folder_path, pdf)
@@ -27,8 +28,5 @@ def merge_pdf(folder_path: str, file_name: str, page: ft.Page):
     merger.close()
 
     page.open(page.controls[-1])
-    page.controls[0].controls[0].value = ""
-    page.controls[1].value = ""
     page.update()
     return
-    
